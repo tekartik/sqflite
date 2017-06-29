8f09fbdaa90168dc5290f7da58388efd36b43bf0
Alexandre Roux Tekartik <alex@tekartik.com>
2017-06-29 17:23:46 +0200

    fix: pub publish warnings on example dependencies

--- example/lib/test_page.dart
+++ example/lib/test_page.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:path/path.dart';
+import 'package:func/func.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'model/item.dart';
