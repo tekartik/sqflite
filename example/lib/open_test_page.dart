8b831555db4c1e317b3c78c0761f091e51b745e8
Alexandre Roux Tekartik <alex@tekartik.com>
2017-08-01 12:05:46 +0200

    test: move exception tests in separate group dur to force debugging from webstorm

--- example/lib/open_test_page.dart
+++ example/lib/open_test_page.dart
@@ -105,99 +105,12 @@ class OpenTestPage extends TestPage {
       await database.close();
     });
 
-    test("Open onDowngrade fail", () async {
-      String path = await initDeleteDb("open_on_downgrade_fail.db");
-      Database database = await openDatabase(path, version: 2,
-          onCreate: (Database db, int version) async {
-        await db.execute("CREATE TABLE Test(id INTEGER PRIMARY KEY)");
-      });
-      await database.close();
-
-      // currently this is crashing...
-      // should fail going back in versions
-      try {
-        database = await openDatabase(path,
-            version: 1, onDowngrade: onDatabaseVersionChangeError);
-        assert(false);
-      } catch (e) {
-        print(e);
-      }
-
-      // should work
-      database = await openDatabase(path,
-          version: 2, onDowngrade: onDatabaseVersionChangeError);
-      print(database);
-      await database.close();
-    });
-
-    test("Open onDowngrade delete", () async {
-      //await Sqflite.setDebugModeOn();
-
-      String path = await initDeleteDb("open_on_downgrade_delete.db");
-      Database database = await openDatabase(path, version: 3,
-          onCreate: (Database db, int version) async {
-        await db.execute("CREATE TABLE Test(id INTEGER PRIMARY KEY)");
-      });
-      await database.close();
-
-      // should fail going back in versions
-      bool onCreated = false;
-      bool onOpened = false;
-      database = await openDatabase(path, version: 2,
-          onCreate: (Database db, int version) {
-        onCreated = true;
-        assert(version == 2);
-      }, onOpen: (Database db) {
-        onOpened = true;
-      }, onDowngrade: onDatabaseDowngradeDelete);
-      await database.close();
-
-      assert(onCreated);
-      assert(onOpened);
-
-      onCreated = false;
-      onOpened = false;
-
-      database = await openDatabase(path, version: 2,
-          onCreate: (Database db, int version) {
-        assert(false, "should not be called");
-      }, onOpen: (Database db) {
-        onOpened = true;
-      }, onDowngrade: onDatabaseDowngradeDelete);
-      assert(onOpened);
-      await database.close();
-    });
-
     test("Open bad path", () async {
       try {
         await openDatabase("/invalid_path");
         assert(false);
       } on DatabaseException catch (e) {
         assert(e.isOpenFailedError());
-      }
-    });
-
-    test("Access after close", () async {
-      String path = await initDeleteDb("access_after_close.db");
-      Database database = await openDatabase(path, version: 3,
-          onCreate: (Database db, int version) async {
-        await db.execute("CREATE TABLE Test(id INTEGER PRIMARY KEY)");
-      });
-      await database.close();
-      try {
-        await database.getVersion();
-        assert(false);
-      } on DatabaseException catch (e) {
-        print(e);
-        assert(e.isDatabaseClosedError());
-      }
-
-      try {
-        await database.setVersion(1);
-        assert(false);
-      } on DatabaseException catch (e) {
-        print(e);
-        assert(e.isDatabaseClosedError());
       }
     });
   }
