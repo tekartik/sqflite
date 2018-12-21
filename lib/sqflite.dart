c8aa00eedfad693ca0892c2496645a3fe3b37662
Marco Scannadinari <m@scannadinari.co.uk>
2018-12-21 18:47:58 +0000

    Expand docs of openDatabase

--- lib/sqflite.dart
+++ lib/sqflite.dart
@@ -307,21 +307,55 @@ abstract class OpenDatabaseOptions {
 
 ///
 /// Open the database at a given path
-/// setting a version is optional
-/// [onCreate],  [onUpgrade], [onDowngrade] are called in a transaction
 ///
-/// [onConfigure] is called when the database connection is being configured,
-/// to enable features such as write-ahead logging or foreign key support.
-/// This method is called before [onCreate], [onUpgrade], [onDowngrade]
+/// [version] (optional) specifies the schema version of the database being
+/// opened. This is used to decide whether to call [onCreate], [onUpgrade],
+/// and [onDowngrade]
 ///
-/// [onOpen] is called after [onCreate], [onUpgrade], [onDowngrade] are called
+/// The optional callbacks are called in the following order:
 ///
-/// When [readOnly] is true all other parameters are ignored and the database
-/// is opened as is
+/// 1. [onConfigure]
+/// 2. [onCreate] or [onUpgrade] or [onDowngrade]
+/// 5. [onOpen]
+///
+/// [onConfigure] is the first callback invoked when opening the database. It
+/// allows you to perform database initialization such as enabling foreign keys
+/// or write-ahead logging
+///
+/// If [version] is specified, [onCreate], [onUpgrade], and [onDowngrade] can
+/// be called. These functions are mutually exclusive — only one of them can be
+/// called depending on the context, although they can all be specified to
+/// cover multiple scenarios
+///
+/// [onCreate] is called if the database did not exist prior to calling
+/// [openDatabase]. You can use the opportunity to create the required tables
+/// in the database according to your schema
+///
+/// [onUpgrade] is called if either of the following conditions are met:
+///
+/// 1. [onCreate] is not specified
+/// 2. The database already exists and [version] is higher than the last
+/// database version
+///
+/// In the first case where [onCreate] is not specified, [onUpgrade] is called
+/// with its [oldVersion] parameter as `0`. In the second case, you can perform
+/// the necessary migration procedures to handle the differing schema
+///
+/// [onDowngrade] is called only when [version] is lower than the last database
+/// version. This is a rare case and should only come up if a newer version of
+/// your code has created a database that is then interacted with by an older
+/// version of your code. You should try to avoid this scenario
+///
+/// [onOpen] is the last optional callback to be invoked. It is called after
+/// the database version has been set and before [openDatabase] returns
+///
+/// When [readOnly] (false by default) is true, all other parameters are
+/// ignored and the database is opened as-is
 ///
 /// When [singleInstance] is true (the default), a single database instance is
-/// returned for a given path and other options are ignore if you call
-/// openDatabase again if the database is already opened
+/// returned for a given path. Subsequent calls to [openDatabase] with the
+/// same path will return the same instance, and will discard all other
+/// parameters such as callbacks for that invocation.
 ///
 Future<Database> openDatabase(String path,
     {int version,
@@ -353,13 +387,14 @@ Future<Database> openReadOnlyDatabase(String path) =>
 ///
 /// Get the default databases location
 ///
-/// on Android, it is typically data/data/<package_name>/databases
-/// on iOS, it is the Documents directory
+/// On Android, it is typically data/data/<package_name>/databases
+///
+/// On iOS, it is the Documents directory
 ///
 Future<String> getDatabasesPath() => databaseFactory.getDatabasesPath();
 
 ///
-/// delete the database at the given path
+/// Delete the database at the given path
 ///
 Future<void> deleteDatabase(String path) =>
     databaseFactory.deleteDatabase(path);
