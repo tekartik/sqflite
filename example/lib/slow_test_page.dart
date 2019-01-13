bdb76eeb8b61cc0410efa3a8e5ddbcc09f8ab363
alex <alex@tekartik.com>
2019-01-13 12:21:01 +0100

    test: simple perf test
    emulator API28 i7 8700K ubuntu 18.04 SSD EVO 970 500GB
    
    sw 0:00:00.624015 insert 10000 items batch
    sw 0:00:00.042323 SELECT * From Test : 10000 items
    sw 0:00:00.041614 SELECT * FROM Test WHERE name LIKE %item% 10000 items
    sw 0:00:00.002500 SELECT * FROM Test WHERE name LIKE %dummy% 0 items

--- example/lib/slow_test_page.dart
+++ example/lib/slow_test_page.dart
@@ -75,5 +75,45 @@ class SlowTestPage extends TestPage {
       print("1000 insert batch no result ${sw.elapsed}");
       await db.close();
     });
+
+    int count = 10000;
+
+    test("Perf $count item", () async {
+      //Sqflite.devSetDebugModeOn(true);
+      String path = await initDeleteDb("pref_${count}_items.db");
+      Database db = await openDatabase(path);
+      try {
+        await db
+            .execute("CREATE TABLE Test (id INTEGER PRIMARY KEY, name TEXT)");
+
+        Stopwatch sw = new Stopwatch()..start();
+        Batch batch = db.batch();
+
+        for (int i = 0; i < count; i++) {
+          batch.rawInsert("INSERT INTO Test (name) VALUES (?)", ["item $i"]);
+        }
+        await batch.commit();
+        print("sw ${sw.elapsed} insert $count items batch ");
+
+        sw = new Stopwatch()..start();
+        var result = await db.query('Test');
+        print(
+            "sw ${sw.elapsed} SELECT * From Test : ${result.length} items");
+
+        sw = new Stopwatch()..start();
+        result =
+            await db.query('Test', where: 'name LIKE ?', whereArgs: ['%item%']);
+        print(
+            "sw ${sw.elapsed} SELECT * FROM Test WHERE name LIKE %item% ${result.length} items");
+
+        sw = new Stopwatch()..start();
+        result = await db
+            .query('Test', where: 'name LIKE ?', whereArgs: ['%dummy%']);
+        print(
+            "sw ${sw.elapsed} SELECT * FROM Test WHERE name LIKE %dummy% ${result.length} items");
+      } finally {
+        await db.close();
+      }
+    });
   }
 }
