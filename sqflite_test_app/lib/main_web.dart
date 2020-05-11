bd705f4ba82daf10d8f8ef814347aefeda665803
alex <alex@tekartik.com>
2020-05-11 10:19:34 +0200

    test: dummy web implementation

---
+++ sqflite_test_app/lib/main_web.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite_example/main.dart' as example;
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  example.main();
+}
---
+++ sqflite_test_app/lib/src/sqflite_web_sim.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:sqflite/sqflite_dev.dart';
+
+import 'database_factory_web.dart';
+
+/// The Web plugin registration.
+///
+/// Define a default `DatabaseFactory`
+class SqflitePluginWeb {
+  /// Registers the default database factory.
+  static void registerWith(Registrar registrar) {
+    /// Set the default database factory to use.
+    /// Currently calling an on-purpose deprecated helper.
+    // ignore: deprecated_member_use
+    setMockDatabaseFactory(databaseFactoryWeb);
+  }
+}
