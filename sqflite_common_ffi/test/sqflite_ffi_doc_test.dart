d21988f9a36a9c13d0db1e041b4ad5fd8e167656
alex <alex@tekartik.com>
2020-05-21 10:50:05 +0200

    doc: unit test

--- sqflite_common_ffi/test/sqflite_ffi_doc_test.dart
+++ sqflite_common_ffi/test/sqflite_ffi_doc_test.dart
@@ -1,10 +1,31 @@
-import 'package:test/test.dart';
 import 'package:sqflite_common/sqlite_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:test/test.dart';
 
 void main() {
   // Init ffi loader if needed.
   sqfliteFfiInit();
+  test('MyUnitTest', () async {
+    var factory = databaseFactoryFfi;
+    var db = await factory.openDatabase(inMemoryDatabasePath);
+
+    // Should fail table does not exists
+    try {
+      await db.query('Test');
+    } on DatabaseException catch (e) {
+      // no such table: Test
+      expect(e.isNoSuchTableError('Test'), isTrue);
+      print(e.toString());
+    }
+
+    // Ok
+    await db.execute('CREATE TABLE Test (id INTEGER PRIMARY KEY)');
+    await db.execute('ALTER TABLE Test ADD COLUMN name TEXT');
+    // should succeed, but empty
+    expect(await db.query('Test'), []);
+
+    await db.close();
+  });
   test('simple sqflite example', () async {
     var db = await databaseFactoryFfi.openDatabase(inMemoryDatabasePath);
     expect(await db.getVersion(), 0);
