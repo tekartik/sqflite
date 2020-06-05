5c2cbdb46eb413f175f6b206d8b51f9f42f12eea
Reni Delonzek <renidelonzek@gmail.com>
2020-06-05 01:41:01 -0300

    fix: path sqlite in windows

--- sqflite_common_ffi/lib/src/windows/setup.dart
+++ sqflite_common_ffi/lib/src/windows/setup.dart
@@ -1,15 +1,22 @@
 import 'dart:ffi';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart' as foundation;
 import 'package:moor_ffi/database.dart';
 import 'package:moor_ffi/open_helper.dart';
 import 'package:path/path.dart';
-import 'package:sqflite_common_ffi/src/windows/setup_impl.dart';
 
 /// On windows load the embedded sqlite3.dll for convenience
 void windowsInit() {
-  var location = findPackagePath(Directory.current.path);
-  var path = normalize(join(location, 'src', 'windows', 'sqlite3.dll'));
+  String rootPath;
+  if (foundation.kDebugMode) {
+    rootPath = 'build';
+  } else {
+    rootPath = 'data';
+  }
+  var location = Directory.current.path;
+  String path = normalize(join(location, rootPath, 'flutter_assets', 'packages', 'sqflite_common_ffi', 'assets', 'sqlite3.dll'));
+  
   open.overrideFor(OperatingSystem.windows, () {
     // devPrint('loading $path');
     try {
