22015be57e371d07dde97175ca8932d9627c0bf6
alex <alex@tekartik.com>
2020-07-16 00:21:22 +0200

    feat: allow ffi import in web app

---
+++ sqflite_common_ffi/lib/src/sqflite_ffi_stub.dart
@@ -0,0 +1,15 @@
+import 'package:sqflite_common/sqlite_api.dart';
+
+/// The database factory to use for ffi.
+///
+/// Check support documentation.
+///
+/// Currently supports Win/Mac/Linux.
+DatabaseFactory get databaseFactoryFfi => throw UnimplementedError(
+    'databaseFactoryFfi only supported for io application');
+
+/// Optional. Initialize ffi loader.
+///
+/// Call in main until you find a loader for your needs.
+void sqfliteFfiInit() => throw UnimplementedError(
+    'sqfliteFfiInit only supported for io application');
