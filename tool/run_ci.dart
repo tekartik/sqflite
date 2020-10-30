2064e92f289cc87e65e4258ed1e00532ff86cc29
alex <alex@tekartik.com>
2020-10-30 15:26:33 +0100

    test: try github actions integration

---
+++ tool/run_ci.dart
@@ -0,0 +1,41 @@
+import 'package:process_run/shell.dart';
+import 'package:dev_test/package.dart';
+
+Future main() async {
+  // await packageRunCi('.');
+
+  var shell = Shell();
+
+  await shell.run('flutter doctor');
+
+  /*
+  for (var dir in [
+    'sqflite/example',
+    'sqflite',
+    'sqflite_test_app',
+  ]) {
+    shell = shell.pushd(dir);
+    await shell.run('''
+    
+    flutter packages get
+    dart tool/travis.dart
+    
+        ''');
+    shell = shell.popd();
+  }
+  */
+  for (var dir in [
+    'sqflite_common',
+    // 'sqflite_common_test',
+    // 'sqflite_common_ffi',
+  ]) {
+    shell = shell.pushd(dir);
+    await shell.run('''
+    
+    pub get
+    dart tool/travis.dart
+    
+        ''');
+    shell = shell.popd();
+  }
+}
