1832ec72e8e45e351b64737e485e6d4eb4f9f176
alex <alex@tekartik.com>
2020-11-20 01:01:34 +0100

    fix: allow running ci on dart/flutter stable (still)

--- sqflite_support/tool/run_ci.dart
+++ sqflite_support/tool/run_ci.dart
@@ -4,8 +4,17 @@
 // This is to allow running this file without null experiment
 // In the future, remove this 2.9 comment or run using: dart --enable-experiment=non-nullable --no-sound-null-safety run tool/travis.dart
 
+import 'dart:io';
+
 import 'package:dev_test/package.dart';
+import 'package:process_run/shell.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 Future main() async {
-  await packageRunCi('.');
+  final nnbdEnabled = dartVersion > Version(2, 12, 0, pre: '0');
+  if (nnbdEnabled) {
+    await packageRunCi('.');
+  } else {
+    stderr.writeln('ci test skipped for $dartVersion');
+  }
 }
