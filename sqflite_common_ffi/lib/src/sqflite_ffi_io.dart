1770221b8f3a090b1ec997c7a6b525817306fb35
David Martos <davidmartos96@gmail.com>
2020-11-25 10:39:01 +0100

    Sample code

--- sqflite_common_ffi/lib/src/sqflite_ffi_io.dart
+++ sqflite_common_ffi/lib/src/sqflite_ffi_io.dart
@@ -16,6 +16,25 @@ DatabaseFactory get databaseFactoryFfi => databaseFactoryFfiImpl;
 /// some behavior with the sqlite3 dynamic library opening. This function should
 /// be either a top level function or a static function.
 /// Prefer the use of the [databaseFactoryFfi] getter if you don't need this functionality.
+/// 
+/// Example for overriding the sqlite library in Windows by providing a custom path.
+///
+/// ```dart
+/// import 'package:sqlite3/open.dart';
+/// 
+/// void ffiInit() {
+///   open.overrideFor(
+///     OperatingSystem.windows,
+///     () => DynamicLibrary.open('path/to/bundled/sqlite.dll'),
+///   );
+/// }
+/// 
+/// Future<void> main() async {
+///   final dbFactory = createDatabaseFactoryFfi(ffiInit: ffiInit);
+///   final db = await dbFactory.openDatabase(inMemoryDatabasePath);
+///   ...
+/// }
+/// ```
 DatabaseFactory createDatabaseFactoryFfi({FFIInit? ffiInit}) {
   return createDatabaseFactoryFfiImpl(ffiInit: ffiInit);
 }
