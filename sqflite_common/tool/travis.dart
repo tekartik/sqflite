83075e13ce7d4f48dbed2ac49532bc7b717bc5ac
alex <alex@tekartik.com>
2021-02-28 10:35:41 +0100

    fix http uri issue

--- sqflite_common/tool/travis.dart
+++ sqflite_common/tool/travis.dart
@@ -26,8 +26,8 @@ pub run test
       try {
         final dir = await Directory.systemTemp.createTemp('sqflite_common');
         bashFilePath = join(dir.path, 'codecov.bash');
-        await File(bashFilePath)
-            .writeAsString(await IOClient().read('https://codecov.io/bash'));
+        await File(bashFilePath).writeAsString(
+            await IOClient().read(Uri.parse('https://codecov.io/bash')));
         await shell.run('bash $bashFilePath');
       } catch (e) {
         stdout.writeln('error $e running $bashFilePath');
