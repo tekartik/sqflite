8ac6d617b678016feaf87fe3c092189eba5b36d3
alex <alex@tekartik.com>
2021-05-28 10:06:42 +0200

    fix: linux setup on CI

--- sqflite_common_ffi/tool/linux_setup.dart
+++ sqflite_common_ffi/tool/linux_setup.dart
@@ -3,7 +3,7 @@ import 'package:process_run/shell_run.dart';
 
 Future main() async {
   if (Platform.isLinux) {
-    // Assuming ubuntu, to run as root
-    await run('apt-get -y install libsqlite3-0 libsqlite3-dev');
+    // Assuming ubuntu, to run as root, this is mainly for CI
+    await run('sudo apt-get -y install libsqlite3-0 libsqlite3-dev');
   }
 }
