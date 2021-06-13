08d14dc924479f51b766f45d252c14f213780335
alex <alex@tekartik.com>
2021-06-13 12:26:40 +0200

    fix: lints on flutter dev

--- sqflite_test_app/test/sqflite_flutter_ffi_test.dart
+++ sqflite_test_app/test/sqflite_flutter_ffi_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 Future main() async {
   // Setup sqflite_common_ffi for flutter test
