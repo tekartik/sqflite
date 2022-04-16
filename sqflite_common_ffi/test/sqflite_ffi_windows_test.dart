71d19e6e6dbc6cb3e0ad2239f28a24439467a7f4
Alexandre Roux <alex@tekartik.com>
2022-04-16 10:32:13 +0200

    test: windows test only

--- sqflite_common_ffi/test/sqflite_ffi_windows_test.dart
+++ sqflite_common_ffi/test/sqflite_ffi_windows_test.dart
@@ -1,3 +1,6 @@
+@TestOn('vm')
+import 'dart:io';
+
 import 'package:sqflite_common/sqlite_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:test/test.dart';
@@ -5,14 +8,16 @@ import 'package:test/test.dart';
 import 'sqflite_ffi_windows_setup_test.dart';
 
 void main() {
-  sqfliteFfiInit();
-  group('windows', () {
-    test('version', () async {
-      final db = await databaseFactoryFfi.openDatabase(inMemoryDatabasePath);
-      final results = await db.rawQuery('select sqlite_version()');
+  if (Platform.isWindows) {
+    sqfliteFfiInit();
+    group('windows', () {
+      test('version', () async {
+        final db = await databaseFactoryFfi.openDatabase(inMemoryDatabasePath);
+        final results = await db.rawQuery('select sqlite_version()');
 
-      var version = results.first.values.first;
-      expect(version, windowsSqliteVersion);
+        var version = results.first.values.first;
+        expect(version, windowsSqliteVersion);
+      });
     });
-  });
+  }
 }
