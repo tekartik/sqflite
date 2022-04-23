54bc63eac4d1e4bff0e79a863665eebb1f2a4b8e
Mark Muhleder <markmuhleder@gmail.com>
2022-04-23 19:34:38 +0100

    Open wasm backed sqlite database on web

--- sqflite_common_ffi/lib/src/sqflite_ffi_impl_web.dart
+++ sqflite_common_ffi/lib/src/sqflite_ffi_impl_web.dart
@@ -1,16 +1,32 @@
-import 'package:sqlite3/common.dart' as common;
+import 'package:http/http.dart' as http;
 
-/// Opens the database using an ffi implementation
-Future<common.CommonDatabase> handleOpenPlatform(Map argumentsMap) async {
-  throw UnimplementedError();
+import 'package:sqlite3/wasm.dart';
+
+/// Opens the database using a wasm implementation
+Future<CommonDatabase> handleOpenPlatform(Map argumentsMap) async {
+  var path = argumentsMap['path'] as String;
+  var readOnly = (argumentsMap['readOnly'] as bool?) ?? false;
+  final response = await http.get(Uri.parse('sqlite3.wasm'));
+  final fs = await IndexedDbFileSystem.load('/');
+  var sqlite = await WasmSqlite3.load(response.bodyBytes, SqliteEnvironment(fileSystem: fs));
+  var mode = readOnly ? OpenMode.readOnly : OpenMode.readWriteCreate;
+  var db = sqlite.open(path, mode: mode);
+  return db;
 }
 
 /// Delete the database file.
 Future<void> deleteDatabasePlatform(String path) async {
-  throw UnimplementedError();
+  final fs = await IndexedDbFileSystem.load('/');
+  fs.deleteFile(path);
 }
 
 /// Check if database file exists
 Future<bool> handleDatabaseExistsPlatform(String path) async {
-  throw UnimplementedError();
+  // Ignore failure
+  try {
+    final fs = await IndexedDbFileSystem.load('/');
+    return fs.exists(path);
+  } catch (_) {
+    return false;
+  }
 }
