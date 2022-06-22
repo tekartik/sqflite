bf086ef54afa459f79536d7e36f2db5a53c5e78d
Aaron Clarke <aaclarke@google.com>
2022-06-22 15:25:05 -0700

    moved diff from macos to ios

--- sqflite/macos/Classes/SqflitePlugin.m
+++ sqflite/macos/Classes/SqflitePlugin.m
@@ -892,6 +892,14 @@ static NSInteger _databaseOpenCount = 0;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    // result wrapper to post the result on the main thread
+    // until background threads are supported for plugin services
+    FlutterResult wrappedResult = ^(id res) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            result(res);
+        });
+    };
+    
     if ([_methodGetPlatformVersion isEqualToString:call.method]) {
 #if TARGET_OS_IPHONE
         result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
@@ -900,23 +908,23 @@ static NSInteger _databaseOpenCount = 0;
         result([@"macOS " stringByAppendingString:[[NSProcessInfo processInfo] operatingSystemVersionString]]);
 #endif
     } else if ([_methodOpenDatabase isEqualToString:call.method]) {
-        [self handleOpenDatabaseCall:call result:result];
+        [self handleOpenDatabaseCall:call result:wrappedResult];
     } else if ([_methodInsert isEqualToString:call.method]) {
-        [self handleInsertCall:call result:result];
+        [self handleInsertCall:call result:wrappedResult];
     } else if ([_methodQuery isEqualToString:call.method]) {
-        [self handleQueryCall:call result:result];
+        [self handleQueryCall:call result:wrappedResult];
     } else if ([_methodUpdate isEqualToString:call.method]) {
-        [self handleUpdateCall:call result:result];
+        [self handleUpdateCall:call result:wrappedResult];
     } else if ([_methodExecute isEqualToString:call.method]) {
-        [self handleExecuteCall:call result:result];
+        [self handleExecuteCall:call result:wrappedResult];
     } else if ([_methodBatch isEqualToString:call.method]) {
-        [self handleBatchCall:call result:result];
+        [self handleBatchCall:call result:wrappedResult];
     } else if ([_methodGetDatabasesPath isEqualToString:call.method]) {
         [self handleGetDatabasesPath:call result:result];
     } else if ([_methodCloseDatabase isEqualToString:call.method]) {
-        [self handleCloseDatabaseCall:call result:result];
+        [self handleCloseDatabaseCall:call result:wrappedResult];
     } else if ([_methodDeleteDatabase isEqualToString:call.method]) {
-        [self handleDeleteDatabaseCall:call result:result];
+        [self handleDeleteDatabaseCall:call result:wrappedResult];
     } else if ([_methodOptions isEqualToString:call.method]) {
         [self handleOptionsCall:call result:result];
     } else if ([_methodDebug isEqualToString:call.method]) {
