75423ac4cad531cc9b86fd6e05b323dbf5c05ee0
Alexandre Roux <alex@tekartik.com>
2022-06-28 05:31:05 +0200

    test: dart script for building ios and macos example

---
+++ sqflite/example/tool/build_macos.dart
@@ -0,0 +1,15 @@
+//
+// @dart = 2.9
+//
+// This is to allow running this file without null experiment
+// In the future, remove this 2.9 command and run using: dart --enable-experiment=non-nullable --no-sound-null-safety run tool/travis.dart
+import 'package:process_run/shell.dart';
+
+Future<void> main() async {
+  await buildMacos();
+}
+
+Future<void> buildMacos() async {
+  final shell = Shell();
+  await shell.run('flutter build macos');
+}
