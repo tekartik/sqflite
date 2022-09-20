37e494b5940f5d944741f2a8d1070faa629c05d9
Alexandre Roux <alex@tekartik.com>
2022-09-20 19:16:38 +0200

    [test] integration test run helper

--- sqflite_test_app/tool/run_integration_test.dart
+++ sqflite_test_app/tool/run_integration_test.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
+
 import 'package:process_run/shell.dart';
 import 'package:pub_semver/pub_semver.dart';
 
 Future<void> main() async {
-  await runIntegrationTest();
+  var deviceId = ShellEnvironment().vars['SQFLITE_TEST_DEVICE_ID'];
+  if (deviceId == null) {
+    // ignore: avoid_print
+    stdout.writeln(
+        'To run on a specific device set SQFLITE_TEST_DEVICE_ID=<deviceId>,'
+        ' for example \'emulator-5554\' typically for android emulator');
+  }
+  await runIntegrationTest(deviceId: deviceId);
 }
 
 Future<void> runIntegrationTest({String? deviceId}) async {
