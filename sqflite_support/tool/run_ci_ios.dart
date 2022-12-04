2ffe8eeef1923f46265d0157ed40263bd84e90c9
alex <alex@tekartik.com>
2022-12-04 12:10:16 +0100

    test: add macos and ios ci build

---
+++ sqflite_support/tool/run_ci_ios.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:process_run/shell_run.dart';
+
+var appExp1Path = join('.dart_tool', 'sqflite_test', 'exp1');
+var appExp2Path = join('.dart_tool', 'sqflite_test', 'exp2');
+
+Future<void> main() async {
+  await createAndBuildIos(appPath: appExp1Path, lang: 'objc');
+  await createAndBuildIos(appPath: appExp2Path, lang: 'swift');
+}
+
+Future<void> createAndBuildIos(
+    {required String appPath, required String lang}) async {
+  var shell = Shell();
+
+  try {
+    await Directory(appPath).delete(recursive: true);
+  } catch (_) {}
+  await shell.run(
+      'flutter create --template app --platforms ios --ios-language $lang ${shellArgument(appPath)}');
+
+  shell = shell.cd(appPath);
+  await fixProject(appPath);
+  await shell.run('flutter build ios --debug --no-codesign');
+}
+
+Future<void> fixProject(String appPath) async {
+  var shell = Shell().cd(appPath);
+  await shell.run(
+      'flutter pub add sqflite_example --path ../../../../sqflite/example');
+  await addDepOverrides(appPath);
+}
+
+Future<void> addDepOverrides(String appPath) async {
+  var overrides = '''
+dependency_overrides:
+  sqflite:
+    path: ../../../../sqflite
+  sqflite_common:
+    path: ../../../../sqflite_common
+''';
+
+  var pubspecFile = File(join(appPath, 'pubspec.yaml'));
+  var content = await pubspecFile.readAsString();
+  content = '$content\n$overrides';
+  await pubspecFile.writeAsString(content);
+}
---
+++ sqflite_support/tool/run_ci_macos.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:process_run/shell_run.dart';
+
+import 'run_ci_ios.dart';
+
+var appExp3Path = join('.dart_tool', 'sqflite_test', 'exp3');
+
+Future<void> main() async {
+  await createAndBuildMacos(appPath: appExp3Path);
+}
+
+Future<void> createAndBuildMacos({required String appPath}) async {
+  var shell = Shell();
+  var create = true;
+  if (create) {
+    try {
+      await Directory(appPath).delete(recursive: true);
+    } catch (_) {}
+    await shell.run(
+        'flutter create --template app --platforms macos ${shellArgument(appPath)}');
+  }
+  shell = shell.cd(appPath);
+  await fixProject(appPath);
+  await shell.run('flutter build macos --debug');
+}
