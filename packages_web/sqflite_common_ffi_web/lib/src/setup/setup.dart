05418f230b76cc21b0bcb079f8210931dd12870a
Alexandre Roux <alex@tekartik.com>
2023-04-11 10:54:12 +0200

    [sqflite_common_ffi_web] v0.3.4 depends on sqlite3 1.11.0

--- packages_web/sqflite_common_ffi_web/lib/src/setup/setup.dart
+++ packages_web/sqflite_common_ffi_web/lib/src/setup/setup.dart
@@ -10,7 +10,7 @@ import 'package:pub_semver/pub_semver.dart';
 import 'package:sqflite_common_ffi_web/src/constant.dart';
 
 // https://github.com/simolus3/sqlite3.dart/releases
-var _sqlite3WasmVersion = Version(1, 10, 0);
+var _sqlite3WasmVersion = Version(1, 11, 0);
 var _sqlite3WasmReleaseUri = Uri.parse(
     'https://github.com/simolus3/sqlite3.dart/releases/download/sqlite3-$_sqlite3WasmVersion/sqlite3.wasm');
 
