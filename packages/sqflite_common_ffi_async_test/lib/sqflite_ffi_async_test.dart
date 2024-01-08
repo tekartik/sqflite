67356b5892d335cccd84ced9200ccba22c3383ad
Alexandre Roux <alex@tekartik.com>
2024-01-08 09:07:05 +0100

    fix: remove obsolete template files, fixes #1071

--- packages/sqflite_common_ffi_async_test/lib/sqflite_ffi_async_test.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/sqflite_ffi_async_test_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
