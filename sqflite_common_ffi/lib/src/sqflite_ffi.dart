aa7e346e6666706cb0915c4a5ff78da7d50f986b
Alexandre Roux <alex@tekartik.com>
2024-03-16 16:46:24 +0100

    [sqflite_common] remove dependency on dart:html

--- sqflite_common_ffi/lib/src/sqflite_ffi.dart
+++ sqflite_common_ffi/lib/src/sqflite_ffi.dart
@@ -1,4 +1,5 @@
-export 'sqflite_ffi_io.dart' if (dart.library.js) 'sqflite_ffi_web.dart';
+export 'sqflite_ffi_io.dart'
+    if (dart.library.js_interop) 'sqflite_ffi_web.dart';
 
 /// Signature responsible for overriding the SQLite dynamic library to use.
 typedef SqfliteFfiInit = void Function();
