e37e8a9e9c62704f24e53f7969f625334c32bf20
Alexandre Roux <alex@tekartik.com>
2025-11-13 19:34:19 +0100

    add test that runs the app directly however still failing when using dart pub global run

--- packages/console_test_app/test/console_test_app_test.dart
+++ packages/console_test_app/test/console_test_app_test.dart
@@ -1,9 +1,20 @@
 import 'dart:io';
 
+import 'package:path/path.dart';
 import 'package:process_run/shell.dart';
 import 'package:test/test.dart';
 
 void main() {
+  test('run directly', () async {
+    var shell = Shell();
+    var bin = normalize(absolute(join('bin', 'sqflite_ffi_simple_bin.dart')));
+    // Try running from a different path
+    var tmpPath = Directory.systemTemp.path;
+    shell = shell.cd(tmpPath);
+    await shell.run('''
+        dart run ${shellArgument(bin)}
+        ''');
+  }, timeout: Timeout(Duration(seconds: 60)));
   test('activate and run', () async {
     var shell = Shell();
     await shell.run('''
@@ -11,6 +22,7 @@ void main() {
         ''');
     var tmpPath = Directory.systemTemp.path;
     shell = shell.cd(tmpPath);
+    // This is failing on CI on dart 3.10.0. Are build hooks ran properly
     await shell.run('''
         dart pub global run sqflite_ffi_console_test_app:sqflite_ffi_simple_bin
         ''');
