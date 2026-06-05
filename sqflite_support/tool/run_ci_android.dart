c342e633ab96b2935df33757908282f5add2fbef
Alexandre Roux <alex@tekartik.com>
2026-06-05 18:14:08 +0200

    add building an app and adding sqflite in android ci

--- sqflite_support/tool/run_ci_android.dart
+++ sqflite_support/tool/run_ci_android.dart
@@ -5,8 +5,10 @@ import 'package:process_run/shell.dart';
 
 late String workDir;
 String get workAndroidDir => join(workDir, 'android');
+var appExpAndroid1Path = join('.dart_tool', 'sqflite_test', 'expandroid1');
 
 Future main() async {
+  await createAddSqfliteAndBuildAndroid(appPath: appExpAndroid1Path);
   workDir = join('..', 'sqflite_android', 'example');
   await buildApk();
   await runAndroidTest();
@@ -31,4 +33,42 @@ Future<void> createAndroidProject() async {
     await Directory(workAndroidDir).delete(recursive: true);
   } catch (_) {}
   await run('flutter create --platforms android ${shellArgument(workDir)}');
+}
+
+Future<void> createAddSqfliteAndBuildAndroid({required String appPath}) async {
+  var shell = Shell();
+
+  try {
+    await Directory(appPath).delete(recursive: true);
+  } catch (_) {}
+  await shell.run(
+    'flutter create --template app --platforms android ${shellArgument(appPath)}',
+  );
+
+  shell = shell.cd(appPath);
+  await fixProject(appPath);
+  await shell.run('flutter build apk');
+}
+
+Future<void> fixProject(String appPath) async {
+  var shell = Shell().cd(appPath);
+  await shell.run(
+    'flutter pub add sqflite_example --path ../../../../sqflite/example',
+  );
+  await addDepOverrides(appPath);
+}
+
+Future<void> addDepOverrides(String appPath) async {
+  var overrides = '''
+dependency_overrides:
+  sqflite:
+    path: ../../../../sqflite
+  sqflite_common:
+    path: ../../../../sqflite_common
+''';
+
+  var pubspecFile = File(join(appPath, 'pubspec.yaml'));
+  var content = await pubspecFile.readAsString();
+  content = '$content\n$overrides';
+  await pubspecFile.writeAsString(content);
 }
